05ced35114b5dafad694ebb5c04fb6e46265efea
BuildTools <unconfigured@null.spigotmc.org>
2020-10-12 15:51:32 +0100

    Add project files.

---
+++ Interpreter/Interpreter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static Lexer;
+using static Parser;
+
+namespace Interpreter
+{
+    class Interpreter
+    {
+        static void Main(string[] args)
+        {
+            int MAX_TOKENS = 16;
+
+
+            while (true)
+            {
+                Console.WriteLine("Try the interpreter by typing in a number ");
+                string input = Console.ReadLine();
+                Console.WriteLine("Input was {0}", input);
+
+                Lexer lex = new Lexer(ref input, MAX_TOKENS);
+
+
+            }
+        }
+    }
+}
---
+++ Interpreter/Lexer.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+public class Lexer
+{
+	string input;
+	int MAX_TOKENS;
+	int current;
+	int ahead;
+
+	struct LookupTable
+	{
+		public int[] symbols;
+		public int[] values;
+
+		public LookupTable(int MAX_TOKENS) {
+			symbols = new int[MAX_TOKENS];
+			values = new int[MAX_TOKENS];
+		}
+	}
+
+	public Lexer(ref string input, int MAX_TOKENS)
+	{
+		this.input=input;
+		this.MAX_TOKENS = MAX_TOKENS;
+		this.current = 0;
+		this.ahead = -1;
+		Process();
+	}
+
+	public int Process()
+	{
+		LookupTable lt = new LookupTable(MAX_TOKENS);
+
+		for (this.current = 0; this.current < this.input.Length; this.current++)
+		{
+			switch (input[current])
+			{
+				case (' '): break;
+
+				case ('*'):
+					Console.WriteLine("match found for * ");
+					break;
+
+				case ('/'):
+					Console.WriteLine("match found for / ");
+					break;
+
+				case ('+'):
+					Console.WriteLine("match found for + ");
+					break;
+
+				case ('-'):
+					Console.WriteLine("match found for - ");
+					break;
+
+				case ('('):
+					Console.WriteLine("match found for ( ");
+					break;
+
+				case (')'):
+					Console.WriteLine("match found for ) ");
+					break;
+
+				default:
+					if (Char.IsDigit(input[current]) == false)
+					{
+						//Console.WriteLine("unkown symbol {0}", currentChar);
+					}
+					else{
+						Console.WriteLine("is digit {0}", input[current]);
+					}
+					break;
+
+			}
+
+		}
+
+
+
+
+
+
+
+
+
+		return 1;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
